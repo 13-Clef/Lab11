9ea903d6603e826c858d25c43f1b90ddeed31754
13-Clef <rocketmail72002@gmail.com>
2022-11-01 18:39:55 +0800

    Update Script
    Added Test Cases 6

--- Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
+++ Crashteroids Starter/Assets/RW/Tests/TestSuite.cs
@@ -99,5 +99,19 @@ namespace Tests
         }
 
 
+        [UnityTest]
+        public IEnumerator DestroyedAsteroidRaisesScore()
+        {
+            // 1
+            GameObject asteroid = game.GetSpawner().SpawnAsteroid();
+            asteroid.transform.position = Vector3.zero;
+            GameObject laser = game.GetShip().SpawnLaser();
+            laser.transform.position = Vector3.zero;
+            yield return new WaitForSeconds(0.1f);
+            // 2
+            Assert.AreEqual(game.score, 1);
+        }
+
+
     }
 }
